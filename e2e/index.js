0146127f6362b188a11b1d14fd07460b8b986786
nateekar <nateekar@adobe.com>
2023-03-13 01:39:57 -0700

    update-4

--- e2e/index.js
+++ e2e/index.js
@@ -3,4 +3,7 @@ const ise2e = require('./ise');
 
 console.log(ise2e('sk-s'))
 console.log(calculator(5, 3, "+")); // Output: 8
-module.exports = {ise2e}
+module.exports = {
+    ise2e,
+    calculator
+}
