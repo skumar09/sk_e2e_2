502e0251865aa1308f42bb806d62d54688c7bd49
nateekar <nateekar@adobe.com>
2023-03-13 12:30:14 -0700

    u5

--- e2e/index.js
+++ e2e/index.js
@@ -1,6 +1,6 @@
-const calculator = require('./libs/calculator');
-const ise2e = require('./libs/ise');
-const spec = require('./libs/parse');
+const { calculator, ise2e, spec} = require('./libs');
+//const ise2e = require('./libs/ise');
+//const spec = require('./libs/parse');
 const { test, expect } = require('@playwright/test');
 
 console.log(ise2e('sk-s'))
