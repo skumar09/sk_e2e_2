b9076783330eed22b33d89330abf42f1046e1236
nateekar <nateekar@adobe.com>
2023-03-12 19:52:32 -0700

    calculator.js

---
+++ calculator.js
@@ -0,0 +1,21 @@
+function calculator(num1, num2, operator) {
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
+    let result;
+  
+    if (operator === "+") {
+      result = num1 + num2;
+    } else if (operator === "-") {
+      result = num1 - num2;
+    } else if (operator === "*") {
+      result = num1 * num2;
+    } else if (operator === "/") {
+      result = num1 / num2;
+    } else {
+      return "Invalid operator";
+    }
+  
+    return result;
+  }
+  
+  module.exports = calculator;
--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 function ise2e(string){
     return string==='sk-e2e'
 }
-module.exports = ise2e
+module.exports = {
+  ise2e,
+  calculator
+}
+
