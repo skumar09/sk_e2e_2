852c07ad2272ffc4b2221e084b69898082ab0db1
nateekar <nateekar@adobe.com>
2023-03-13 16:34:20 -0700

    u

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const calculator = require('./e2e/libs/calculator');
 const ise2e = require('./e2e/libs/ise');
 const spec= require('./e2e/libs/parse');
-//const { test, expect } = require('@playwright/test');
+const { test, expect } = require('@playwright/test');
 
 
-console.log(ise2e('sk-s')) // false:
+console.log('Module ise2e:', ise2e('sk-s')) // false:
 
-console.log(calculator(5, 3, "+")); // Output: 8
+console.log('Module Calculator: ',calculator(5, 3, "+")); // Output: 8
 
-console.log(spec.name);
+console.log('--------:',spec.name);
 
 
 module.exports = {
