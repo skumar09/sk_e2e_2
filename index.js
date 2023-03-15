bc144fe9989efecda6665a74f242bf0476342284
nateekar <nateekar@adobe.com>
2023-03-15 00:20:17 -0700

    iindex

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const ise2e = require('./e2e/libs/ise');
 const spec= require('./e2e/libs/parse');
 const { test, expect, defineConfig} = require('@playwright/test');
 
-console.log('Module ise2e:', ise2e('sk-s')) // false:
+//console.log('Module ise2e:', ise2e('sk-s')) // false:
 
-console.log('Module Calculator: ',calculator(5, 3, "+")); // Output: 8
+//console.log('Module Calculator: ',calculator(5, 3, "+")); // Output: 8
 
-console.log('--------:',spec.name);
+console.log('--------:E2E Playwright Execution Started--------------');
 
 
 module.exports = {
